2fff0d2382da6603b6ee0b2cb717ebc6e0f04ccb
Shrikant Kumre <shrikantkumre2001@gmail.com>
2024-03-02 18:01:25 +0530

    Random task selector

---
+++ script.js
@@ -0,0 +1,46 @@
+const tagsContainer = document.querySelector('.tags')
+const textarea = document.querySelector('textarea')
+
+function textChange(ele) {
+    const string = ele.target.value.trim();
+    const tagItemsArray = string.split(',').filter(value => value !== '')
+
+    tagsContainer.innerHTML = '';
+
+    tagItemsArray.forEach((value) => {
+        const tagElemtent = document.createElement('div');
+        tagElemtent.classList.add('tag')
+        tagElemtent.innerText = value;
+        tagsContainer.appendChild(tagElemtent)
+    })
+
+
+    if(ele.key == "Enter"){
+        const changeInterval = setInterval(() => {
+            const randomEle = getRandomElement();
+            randomEle.classList.add('highlight')
+
+            setTimeout(()=>{
+                randomEle.classList.remove('highlight')
+            }, 100)
+        }, 100);
+
+        setTimeout(() => {
+            clearInterval(changeInterval);
+            const randomEle = getRandomElement();
+            randomEle.classList.add('highlight')
+        }, 5000);
+    }
+
+
+
+}
+
+textarea.addEventListener('keyup', textChange);
+
+
+function getRandomElement(){
+    const allTags = document.querySelectorAll('.tag')
+    const randomElement = allTags[Math.round(Math.random() * allTags.length)]
+    return randomElement;
+}
